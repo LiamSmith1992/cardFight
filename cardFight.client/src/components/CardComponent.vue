58a2f9d7a4dc443b02ab8ef5851ebc349ec4ace2
LiamSmith1992 <liamj.smith@hotmail.com>
2023-04-13 09:14:18 -0600

    card component

---
+++ cardFight.client/src/components/CardComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="component">
+    <section class="row">
+      <div v-for="c in cards" class="col-3 card rounded">
+
+      </div>
+    </section>
+
+  </div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+import CardForm from "../pages/CardForm.vue";
+export default {
+  props: {
+    cards: { type: Array, required: true }
+  },
+  setup(props) {
+
+
+
+
+
+    return {}
+  }
+};
+</script>
+
+
+<style lang="scss" scoped></style>
