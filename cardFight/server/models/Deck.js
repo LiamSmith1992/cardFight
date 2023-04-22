ed0b2579b33823355bce9e13cbe1e1fcd72f65d6
LiamSmith1992 <liamj.smith@hotmail.com>
2023-04-22 13:01:56 -0600

    deck model

---
+++ cardFight/server/models/Deck.js
@@ -0,0 +1,23 @@
+import { Schema } from "mongoose";
+
+export const DeckSchema = new Schema({
+
+  name: { type: String, required: true, minLength: 1, maxLength: 20 },
+  cards: { type: Array, },
+  points: { type: Number, maximum: 200 },
+  picture: { type: String, required: true, maxLength: 400 },
+
+},
+  {
+    timestamps: true, toJSON: {
+      virtuals: true
+    }
+  }
+)
+
+DeckSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Account'
+})
