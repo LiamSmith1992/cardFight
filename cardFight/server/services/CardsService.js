f544e3b2b8cfb13c99587baababf7d4c5a26227c
LiamSmith1992 <liamj.smith@hotmail.com>
2023-04-22 13:41:28 -0600

    .put card

--- cardFight/server/services/CardsService.js
+++ cardFight/server/services/CardsService.js
@@ -2,6 +2,7 @@ import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js"
 import { dbContext } from "../db/DbContext.js"
 class CardsService {
 
+
   async createCard(body) {
     const card = await dbContext.Cards.create(body)
     return card
@@ -18,7 +19,20 @@ class CardsService {
     return 'Card has been deleted.'
   }
 
+  async editCard(cardId, cardData) {
+    const original = await dbContext.Cards.findById(cardId)
+    original.name = cardData.name !== undefined ? cardData.name : original?.name
+    original.type = cardData.type !== undefined ? cardData.type : original?.type
+    original.health = cardData.health !== undefined ? cardData.health : original?.health
+    original.strength: cardData.strength !== undefined ? cardData.strength : original?.strength
+    original.defense = cardData.defense !== undefined ? cardData.defense : original?.defense
+    original.magic = cardData.magic !== undefined ? cardData.magic : original.magic
+    original.ability = cardData.ability !== undefined ? cardData.ability : original?.ability
+    original.picture = cardData.picture !== undefined ? cardData.picture : original?.picture
 
+    await original?.save()
+    return original
+  }
 
 
 }
