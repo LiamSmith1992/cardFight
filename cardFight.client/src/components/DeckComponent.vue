e1fe53c1ff6fc465b741e8a44fe804d46a93a02e
LiamSmith1992 <liamj.smith@hotmail.com>
2023-05-01 14:52:08 -0600

    deck display is displaying needs tlc

---
+++ cardFight.client/src/components/DeckComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-for="d in decks">
+    <div class="">
+
+      <h3 class="text-center">{{ d.name }}</h3>
+      <div class="card rounded ">
+        <img class="" :src="d.picture" alt="">
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+export default {
+  setup() {
+
+
+
+    return {
+      decks: computed(() => AppState.decks)
+
+
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped></style>
