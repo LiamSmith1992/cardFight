abe659af540df180fd8dbd95ca21858624ae51d6
LiamSmith1992 <liamj.smith@hotmail.com>
2023-05-09 11:42:26 -0600

    i broke the card form.....

--- cardFight.client/src/pages/CardForm.vue
+++ cardFight.client/src/pages/CardForm.vue
@@ -32,47 +32,91 @@
 
     <label for="mySelect">Ability:</label>
     <select v-model="editable.ability" id="mySelect" name="mySelect">
-      <option value="none">None</option>
-      <option value="peirce">Peirce</option>
-      <!-- Knight, Ninja           ignore defense -->
-      <option value="heal">Heal</option>
-      <!-- Mage, Ghost                     heal a ally 5pt -->
-      <option value="combust">Combust</option>
-      <!-- Witch, Cyborg, mage              does damage equal to half magic to all defenders 1-20 to do 2x card dies-->
-      <option value="debuff">Debuff</option>
-      <!-- Undead, Witch                 lowers stats of select card-->
-      <option value="charge">Charge</option>
-      <!-- Knight, Beast           if two or more cards use charge on turn ignore 2ish defense points-->
-      <option value="sneak">Sneak</option>
-      <!-- Ninja, Beast            cannot be hit on defensive turn must attack on next turn-->
-      <option value="vanish">Vanish</option>
-      <!-- Ghost, ninja                  hide from table can be activated on summon-->
-      <option value="find">Find</option>
-      <!-- Cyborg, Beast           one in 10 chance to grab card from hiding card cannot attack next turn-->
-      <option value="summon">Summon</option>
-      <!-- Mage, Ghost               1 in 20 summon a demon to fight with no armor but health and attack is 2x of magic from summoner-->
-      <option value="poison">Poison</option>
-      <!-- Undead,  witch           takes health equal to poison every other turn-->
-      <option value="bash">Bash</option>
-      <!-- Knight, Cyborg, undead              extra damage if critical stuns enemy 1 in 10 chance-->
+      <div v-if="editable.type != ''">
+        <div v-if="editable.type == 'human'">
+          <option value="none">None</option>
+        </div>
+        <div v-if="editable.type == 'knight' | 'ninja'">
+          <option value="peirce">Peirce</option>
+          <!-- Knight, Ninja           ignore defense -->
+        </div>
+        <div v-if="editable.type == 'mage' | 'ghost'">
+          <option value="heal">Heal</option>
+          <!-- Mage, Ghost                     heal a ally 5pt -->
+        </div>
+        <div v-if="editable.type == 'witch' | 'cyborg' | 'mage'">
+          <option value="combust">Combust</option>
+          <!-- Witch, Cyborg, mage              does damage equal to half magic to all defenders 1-20 to do 2x card dies-->
+        </div>
+        <div v-if="editable.type == 'undead' | 'witch'">
+          <option value="debuff">Debuff</option>
+          <!-- Undead, Witch                 lowers stats of select card-->
+        </div>
+        <div v-if="editable.type == 'knight' | 'beast'">
+          <option value="charge">Charge</option>
+          <!-- Knight, Beast           if two or more cards use charge on turn ignore 2ish defense points-->
+        </div>
+        <div v-if="editable.type == 'ninja' | 'beast'">
+          <option value="sneak">Sneak</option>
+          <!-- Ninja, Beast            cannot be hit on defensive turn must attack on next turn-->
+        </div>
+        <div v-if="editable.type == 'ghost' | 'ninja'">
+          <option value="vanish">Vanish</option>
+          <!-- Ghost, ninja                  hide from table can be activated on summon-->
+        </div>
+        <div v-if="editable.type == 'cyborg' | 'beast'">
+          <option value="find">Find</option>
+          <!-- Cyborg, Beast           one in 10 chance to grab card from hiding card cannot attack next turn-->
+        </div>
+        <div v-if="editable.type == 'mage' | 'ghost'">
+          <option value="summon">Summon</option>
+          <!-- Mage, Ghost               1 in 20 summon a demon to fight with no armor but health and attack is 2x of magic from summoner-->
+        </div>
+        <div v-if="editable.type == 'undead' | 'witch'">
+          <option value="poison">Poison</option>
+          <!-- Undead,  witch           takes health equal to poison every other turn-->
+        </div>
+        <div v-if="editable.type == 'knight' | 'cyborg' | 'undead'">
+          <option value="bash">Bash</option>
+          <!-- Knight, Cyborg, undead              extra damage if critical stuns enemy 1 in 10 chance-->
+        </div>
 
 
-      <option value="defend">Defend</option>
-      <!-- Knight                  if multiple knights all get +1 armor and cannot be ignored-->
-      <option value="stun">Stun</option>
-      <!-- Gohst               card is frozen for one turn-->
-      <option value="boost">Boost</option>
-      <!-- Mage                    adds points to allies-->
-      <option value="assassinate">Assassinate</option>
-      <!-- ninja                 makes card return to hand-->
-      <option value="bite">Bite</option>
-      <!-- undead                   10% chance to turn card into undead with the same stats as biting undead-->
-      <option value="beastMode">Beast Mode</option>
-      <!-- beast                   1 in ten chance for defense and attack to go up 3 points for one round-->
-      <option value="laser">Laser</option>
-      <!-- cyborg                 1 in 20 chance to one shot kill any enemy or do 2x the damage strength to player if no defenders -->
-      <option value="curse">Curse</option>
-      <!-- witch                1 in 15 chance all stats are set to the lowest stat for two turns -->
+        <!-- NOTE this is specials for class -->
+
+        <div v-if="editable.type == 'knight'">
+          <option value="defend">Defend</option>
+          <!-- Knight                  if multiple knights all get +1 armor and cannot be ignored-->
+        </div>
+        <div v-if="editable.type == 'ghost'">
+          <option value="stun">Stun</option>
+          <!-- Gohst               card is frozen for one turn-->
+        </div>
+        <div v-if="editable.type == 'mage'">
+          <option value="boost">Boost</option>
+          <!-- Mage                    adds points to allies-->
+        </div>
+        <div v-if="editable.type == 'ninja'">
+          <option value="assassinate">Assassinate</option>
+          <!-- ninja                 makes card return to hand-->
+        </div>
+        <div v-if="editable.type == 'undead'">
+          <option value="bite">Bite</option>
+          <!-- undead                   10% chance to turn card into undead with the same stats as biting undead-->
+        </div>
+        <div v-if="editable.type == 'beast'">
+          <option value="beastMode">Beast Mode</option>
+          <!-- beast                   1 in ten chance for defense and attack to go up 3 points for one round-->
+        </div>
+        <div v-if="editable.type == 'cyborg'">
+          <option value="laser">Laser</option>
+          <!-- cyborg                 1 in 20 chance to one shot kill any enemy or do 2x the damage strength to player if no defenders -->
+        </div>
+        <div v-if="editable.type == 'witch'">
+          <option value="curse">Curse</option>
+          <!-- witch                1 in 15 chance all stats are set to the lowest stat for two turns -->
+        </div>
+      </div>
 
 
 
